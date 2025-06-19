b0ec10b1f7645684bdd234afc5b254eec77603bf
alexanderjireland <irelandalexander1@gmail.com>
2025-06-18 20:35:12 -0500

    Still working to get draft_env working

--- notebooks/custom-environment/env/draft_environment.py
+++ notebooks/custom-environment/env/draft_environment.py
@@ -19,11 +19,12 @@ class DraftEnvironment(AECEnv):
         "name": "custom_environment_v0",
     }
 
-    def __init__(self, player_df:pd.DataFrame, num_teams=2, draft_type=None, rounds=14, random_pool_size=100):
+    def __init__(self, player_df:pd.DataFrame, num_teams=2, draft_type=None, rounds=14, random_pool_size=100, real_scores_at_draft_end=False):
         super().__init__()
         
         self.player_df = player_df
         self.random_pool_size = random_pool_size
+        self.real_scores = real_scores_at_draft_end
 
         # Initialize environment parameters
         self.num_teams = num_teams
@@ -39,14 +40,14 @@ class DraftEnvironment(AECEnv):
             'BENCH': 7,
         }
 
-        self.possible_starting_positions = ['QB', 'RB', 'WR', 'TE']
+        self.draftable_positions = ['QB', 'RB', 'WR', 'TE']
         self.flex_positions = ['RB', 'WR', 'TE']
 
         self._initialize_agents()
         self._initialize_player_metadata()
         self._initialize_spaces()
 
-        self.position_str_to_index = {pos: i for i, pos in enumerate(self.possible_starting_positions)}
+        self.position_str_to_index = {pos: i for i, pos in enumerate(self.draftable_positions)}
 
         # Collect all available players
         self.available_players = self.player_pool.copy()
@@ -64,14 +65,14 @@ class DraftEnvironment(AECEnv):
 
         self.pos_dqs = {}
 
-        for pos in self.possible_starting_positions:
+        for pos in self.draftable_positions:
              player_id_and_projections = self._sort_and_create_dq(pos)
              diffs = self._create_diffs_dq(player_id_and_projections)
              self.pos_dqs[pos] = PositionDQ(players=player_id_and_projections, diffs=diffs)
 
     def _sort_and_create_dq(self, pos):
-            if pos not in self.possible_starting_positions:
-                 raise ValueError(f"{pos} not in possible starting positions: {self.possible_starting_positions}")
+            if pos not in self.draftable_positions:
+                 raise ValueError(f"{pos} not in possible starting positions: {self.draftable_positions}")
             return deque(sorted([(id, self.gsis_to_projections[id]) for id in self.pos_player_pool[pos]],
                                  key=lambda x: x[1],
                                  reverse=True))
@@ -117,23 +118,17 @@ class DraftEnvironment(AECEnv):
 
         self.team_rosters = {agent: [] for agent in self.possible_agents}
         self.team_positions = {agent: {pos: 0 for pos in self.position_limits} for agent in self.possible_agents}
-        self.team_positions_roster = {
+        self.team_positions_roster = { # Can this one be removed?
             agent: {
                 pos: [None] * self.position_limits[pos]
                 for pos in self.position_limits
             }
             for agent in self.agents
         }
-        self.team_positions_projections = {
+
+        self.team_positions_counts = {
             agent: {
-                pos: [0] * self.position_limits[pos]
-                for pos in self.position_limits
-            }
-            for agent in self.agents
-        }
-        self.team_positions_available = {
-            agent: {
-                pos: 1 for pos in self.position_limits
+                pos: 0 for pos in self.draftable_positions
             }
             for agent in self.agents
         }
@@ -159,12 +154,65 @@ class DraftEnvironment(AECEnv):
             super()._was_dead_step(action)
             return
         
-        player = self._get_player_fom_action(action)
-        if player and self._draft_player(agent, player):
-            self._advance_draft(agent, player)
+        player = self._get_player_from_action(action)
+        if player and self._draft_player(agent, action, player):
+            self._advance_draft(agent, action)
         else:
-            self.rewards[agent] = 0 # or -1 if we want to penalize invalid picks
+            self._advance_draft(agent, action=None)
             tqdm.write(f"[Invalid Pick] {agent} attempted invalid selection (action={action}). Needs to retry.")
+
+
+
+    def _get_player_from_action(self, action):
+        # returns player id for action/position selection
+        # action is an int between 0 and 3 for the four draftable positions
+        position = self.draftable_positions[action]
+        player_queue = self.pos_dqs[position].players
+        if not player_queue:
+            return None
+        
+        player_id, _ = player_queue[0]
+        return player_id
+
+
+
+    def _draft_player(self, agent, action, player):
+        # Ensure the player is valid and available
+        if player not in self.available_players:
+            return False
+        
+        self.team_rosters[agent].append(player)
+        player_pos = self.draftable_positions[action]
+        self.team_positions_counts[agent][player_pos] += 1
+
+        # Remove the player from available players and update draft history
+        self.available_players.remove(player)
+        self.draft_history.append((agent, player))
+        self._get_draft_pick_reward(agent, action)
+
+        return True
+    
+    def _advance_draft(self, action):
+        # Advance the draft to next pick
+        self.current_pick += 1
+        if self.current_pick >= self.total_picks:
+            self._finalize_draft()
+        if action:
+            position = self.draftable_positions[action]
+            self._update_pos_dqs(position)
+            
+        # Move to the next agent in the draft order
+        self.agent_selection = self.current_agent() # Current agent now next agent since current pick has advanced
+
+    def _finalize_draft(self):
+        self.full_roster_df = self._get_full_roster_df()
+        optimized_scores = self.full_roster_df.groupby('agent').apply(self._get_optimized_score)
+        self.optimized_lineups = self.full_roster_df.groupby('agent').apply(self._get_optimized_lineup)
+        for agent in self.possible_agents:
+            self.terminations[agent] = True
+            self.rewards[agent] += optimized_scores[agent]
+        print(f"_finalize_draft: agents = {self.agents}")
+        print(f"_finalize_draft: possible_agents = {self.possible_agents}")
 
     def observe(self, agent):
         return {
@@ -177,36 +225,70 @@ class DraftEnvironment(AECEnv):
                 "difference_with_current_worst_starter": self._get_diff_with_current_worst_starter(agent)
         }
     
+    def render(self):
+        round_num = self.current_pick // self.num_teams + (1 if self.current_pick % self.num_teams != 0 else 0)
+        print(f"\n--- Round {round_num} ---")       
+        named_rosters = self._get_named_team_positions_roster()
+        for agent in self.possible_agents:
+            roster_names = named_rosters[agent]
+            print(f"{agent} roster: {roster_names}")
+        print(f"Remaining players: {len(self.available_players)}")
+
+    def observation_space(self, agent):
+        return self._observation_spaces[agent]
+
+    def action_space(self, agent):
+        return self._action_spaces[agent]
+    
+    def current_agent(self):
+        if self.current_pick >= self.total_picks:
+            return None
+        agent_index = self.draft_order[self.current_pick]
+        return self.possible_agents[agent_index]
+    
     def _get_hurt_score(self, agent):
         diffs = self._get_difference_with_replacement()
         next_opponent_needs = self._get_next_opponent_needs(agent) # returns binary array
         return np.multiply(diffs, next_opponent_needs)
 
     def _get_difference_with_replacement(self):
-        return [self.pos_dqs[pos].diffs[0] for pos in self.possible_starting_positions]
+        return [self.pos_dqs[pos].diffs[0] for pos in self.draftable_positions]
     
     def _get_top_pos_proj_pts(self):
-        return [self.pos_dqs[pos].players[0][1] for pos in self.possible_starting_positions]
+        return [self.pos_dqs[pos].players[0][1] for pos in self.draftable_positions]
     
     def _get_team_needs(self, agent):
-        if self.team_positions_available[agent]['FLEX'] & all(self.team_positions_available[agent][flex_pos]==0 for flex_pos in self.flex_positions):
-            return [self.team_positions_available[agent]['QB'], 1, 1, 1]
-        return [self.team_positions_available[agent][pos] for pos in self.possible_starting_positions]
-    
+        needs = np.zeros(len(self.draftable_positions))
+        counts = self.team_positions_counts[agent]
+
+        if counts['QB'] < self.position_limits['QB']:
+            needs[self.position_str_to_index['QB']] = 1
+        
+        # Determine if FLEX is already filled by how many RBs, WRs, and TEs we have
+        excess_pos = sum([max(0, counts[pos] - self.position_limits[pos]) for pos in self.flex_positions])
+        flex_room = excess_pos < self.position_limits['FLEX']
+
+        for pos in self.flex_positions:
+            has_pos_need = counts[pos] < self.position_limits[pos]
+            if has_pos_need or flex_room:
+                needs[self.position_str_to_index[pos]] = 1
+        return needs
+
     def _get_next_opponent_needs(self, agent):
         next_agent = self._get_next_opponent(agent)
         return self._get_team_needs(next_agent)
     
     def _get_next_opponent(self, agent):
-        agent = int(agent.str.replace('team_', ''))
-        next_agent = agent
-        pick_num = self.current_pick
-        while next_agent == agent and pick_num < self.total_picks:
-            next_agent = self.draft_order[pick_num+1]
-        return f'team_{next_agent}'
+        for i in range(self.current_pick + 1, self.total_picks):
+            next_drafter_index = self.draft_order[i]
+            next_drafter_agent = self.possible_agents[next_drafter_index]
+            if next_drafter_agent != agent:
+                return next_drafter_agent
+        print(f"Oh no! Couldn't find next opponent...")
+        return agent
     
     def _get_available_pos(self):
-        return [int(len(self.pos_dqs[pos].players)>0) for pos in self.possible_starting_positions]
+        return [int(len(self.pos_dqs[pos].players)>0) for pos in self.draftable_positions]
     
     def _get_diff_with_current_worst_starter(self, agent):
         # Need to handle FLEX as well
@@ -215,7 +297,7 @@ class DraftEnvironment(AECEnv):
             flex_proj_pts = self.team_positions_projections[agent]['FLEX']
             agent_roster_worst_pos = [min(self.team_positions_projections[agent]['QB']), flex_proj_pts, flex_proj_pts, flex_proj_pts]
         else:
-            for pos in self.possible_starting_positions:
+            for pos in self.draftable_positions:
                 agent_roster_worst_pos.append(min(self.team_positions_projections[agent][pos]))
         return np.array(self._get_top_pos_proj_pts()) - np.array(agent_roster_worst_pos)
     
@@ -234,7 +316,70 @@ class DraftEnvironment(AECEnv):
         else:
             draft_order = list(range(self.num_teams)) * self.max_rounds
         return draft_order
+    
+    def _update_available_positions(self, agent):
+        flex_room = self.team_positions[agent]['FLEX'] < self.position_limits['FLEX']
+        bench_room = self.team_positions[agent]['BENCH'] < self.position_limits['BENCH']
 
-    #####################
+        if not flex_room:
+            self.team_positions_available[agent]['FLEX'] = 0
+        if not bench_room:
+            self.team_positions_available[agent]['BENCH'] = 0
+        
+        for pos in ['QB', 'RB', 'WR', 'TE']:
+            is_flex_eligible = pos != 'QB'
 
-    #[0, 1, 1, 1] displayed for FLEX
+            if not (self.team_positions[agent][pos] < self.position_limits[pos] or
+                    (is_flex_eligible and flex_room) or bench_room):
+                self.team_positions_available[agent][pos] = 0
+
+
+
+    def _get_full_roster_df(self):
+        rows = []
+        for agent, players in self.team_rosters.items():
+            for gsis_id in players:
+                rows.append({"agent": agent, "gsis_id": gsis_id})
+        roster_df = pd.DataFrame(rows)
+        return roster_df.merge(self.player_df[["gsis_id", "player_name", "position", "fantasy_pts"]], on="gsis_id", how="left")
+    
+    def _get_optimized_lineup(self, df):
+        real_scores = self.real_scores
+        # Add here logic for taking samples from posterior if real_scores = False
+        lineup = []
+        for pos, limit in self.position_limits.items():
+            if pos not in ['FLEX', 'BENCH']:
+                top_players = df[df['position']==pos].nlargest(limit, 'fantasy_pts')
+                lineup.append(top_players)
+        
+        used_ids = pd.concat(lineup)['gsis_id']
+        
+        flex_pool = df[(df['position'].isin(["RB", "WR", "TE"])) & (~df['gsis_id'].isin(used_ids))]
+        lineup.append(flex_pool.nlargest(self.position_limits['FLEX'], 'fantasy_pts'))
+        return pd.concat(lineup)
+    
+    def _get_optimized_score(self, df):
+        lineup = self._get_optimized_lineup(df)
+        return lineup['fantasy_pts'].sum()
+    
+    def _get_value_over_replacement(self, action):
+        return self._get_difference_with_replacement()[action]
+    
+    def _get_draft_pick_reward(self, agent, action):
+        # Value Over Replacement + Hurt Score
+        value_over_replacement = self._get_value_over_replacement(action)
+        hurt_score = self._get_hurt_score(agent)[action]
+        in_draft_reward = value_over_replacement + hurt_score
+        self.rewards[agent] += in_draft_reward
+
+           
+
+    
+    def _get_named_team_positions_roster(self):
+        return {
+            agent: {
+                pos: [self.gsis_to_name.get(pid) if pid is not None else None for pid in players]
+                for pos, players in positions.items()
+            }
+            for agent, positions in self.team_positions_roster.items()
+        }
