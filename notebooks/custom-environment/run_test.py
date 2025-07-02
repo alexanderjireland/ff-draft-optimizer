2233f682125938855ca22a885621190a6f527ec6
alexanderjireland <irelandalexander1@gmail.com>
2025-07-01 20:00:43 -0500

    Merging gitignore

---
+++ notebooks/custom-environment/run_test.py
@@ -0,0 +1,31 @@
+from custom_environment import CustomEnvironment
+from policies import BasicHeuristicPolicy
+import pandas as pd
+
+players = pd.read_csv('data\player_projections\model_06_12_predictions.csv')
+players_2023 = players[players['season'] == 2023]
+
+env = CustomEnvironment(players_2023, num_teams=2, draft_type='snake', rounds=14)
+num_players = len(env.player_pool)
+num_episodes = 1
+
+agents = env.possible_agents
+num_agents = len(agents)
+policies = {agent: BasicHeuristicPolicy(env, num_agents) for agent in agents}
+
+for episode in range(num_episodes):
+    env.reset()
+    while env.agent_selction is not None:
+        agent = env.agent_selection
+        action = policies[agent].select_action(env)
+        obs = env.observe(agent)
+        prev_pick = env.current_pick
+        env.step(action)
+        reward = env.rewards[agent]
+        if env.current_pick > prev_pick:
+            policies[agent].update(action, reward)
+        
+    if episode % 10 == 0:
+        print(f"[Episode {episode}] Scores: {[env.rewards[a] for a in agents]}")
+
+
